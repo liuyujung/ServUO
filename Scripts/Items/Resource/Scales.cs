4d667390d73776c0b372740c2d66a1ce64130d8c
Allan Liu <shopton@gmail.com>
2017-02-09 10:09:53 -0500

    added intermediate commits

--- Scripts/Items/Resource/Scales.cs
+++ Scripts/Items/Resource/Scales.cs
@@ -110,6 +110,7 @@ namespace Server.Items
         public RedScales(int amount)
             : base(CraftResource.RedScales, amount)
         {
+			Name = "Red Scales"; //daat99 OWLTR - custom resource name
         }
 
         public RedScales(Serial serial)
@@ -146,6 +147,7 @@ namespace Server.Items
         public YellowScales(int amount)
             : base(CraftResource.YellowScales, amount)
         {
+			Name = "Yellow Scales"; //daat99 OWLTR - custom resource name
         }
 
         public YellowScales(Serial serial)
@@ -182,6 +184,7 @@ namespace Server.Items
         public BlackScales(int amount)
             : base(CraftResource.BlackScales, amount)
         {
+			Name = "Black Scales"; //daat99 OWLTR - custom resource name
         }
 
         public BlackScales(Serial serial)
@@ -218,6 +221,7 @@ namespace Server.Items
         public GreenScales(int amount)
             : base(CraftResource.GreenScales, amount)
         {
+			Name = "Green Scales"; //daat99 OWLTR - custom resource name
         }
 
         public GreenScales(Serial serial)
@@ -254,6 +258,7 @@ namespace Server.Items
         public WhiteScales(int amount)
             : base(CraftResource.WhiteScales, amount)
         {
+			Name = "White Scales"; //daat99 OWLTR - custom resource name
         }
 
         public WhiteScales(Serial serial)
@@ -290,6 +295,7 @@ namespace Server.Items
         public BlueScales(int amount)
             : base(CraftResource.BlueScales, amount)
         {
+			Name = "Blue Scales"; //daat99 OWLTR - custom resource name
         }
 
         public BlueScales(Serial serial)
@@ -318,4 +324,100 @@ namespace Server.Items
             int version = reader.ReadInt();
         }
     }
+
+	public class CopperScales : BaseScales
+	{
+		[Constructable]
+		public CopperScales() : this(1)
+		{
+		}
+
+		[Constructable]
+		public CopperScales(int amount) : base(CraftResource.CopperScales, amount)
+		{
+			Name = "Copper Scales"; //daat99 OWLTR - custom resource name
+		}
+
+		public CopperScales(Serial serial) : base(serial)
+		{
+		}
+
+		public override void Serialize(GenericWriter writer)
+		{
+			base.Serialize(writer);
+
+			writer.Write((int)0); // version
+		}
+
+		public override void Deserialize(GenericReader reader)
+		{
+			base.Deserialize(reader);
+
+			int version = reader.ReadInt();
+		}
+	}
+
+	public class SilverScales : BaseScales
+	{
+		[Constructable]
+		public SilverScales() : this(1)
+		{
+		}
+
+		[Constructable]
+		public SilverScales(int amount) : base(CraftResource.SilverScales, amount)
+		{
+			Name = "Silver Scales"; //daat99 OWLTR - custom resource name
+		}
+
+		public SilverScales(Serial serial) : base(serial)
+		{
+		}
+
+		public override void Serialize(GenericWriter writer)
+		{
+			base.Serialize(writer);
+
+			writer.Write((int)0); // version
+		}
+
+		public override void Deserialize(GenericReader reader)
+		{
+			base.Deserialize(reader);
+
+			int version = reader.ReadInt();
+		}
+	}
+
+	public class GoldScales : BaseScales
+	{
+		[Constructable]
+		public GoldScales() : this(1)
+		{
+		}
+
+		[Constructable]
+		public GoldScales(int amount) : base(CraftResource.GoldScales, amount)
+		{
+			Name = "Gold Scales"; //daat99 OWLTR - custom resource name
+		}
+
+		public GoldScales(Serial serial) : base(serial)
+		{
+		}
+
+		public override void Serialize(GenericWriter writer)
+		{
+			base.Serialize(writer);
+
+			writer.Write((int)0); // version
+		}
+
+		public override void Deserialize(GenericReader reader)
+		{
+			base.Deserialize(reader);
+
+			int version = reader.ReadInt();
+		}
+	}
 }
